d1fb9c228416da7f3608db36c99abf477089b6a9
Alexei <lexas427@gmail.com>
2022-06-03 00:09:52 +0300

    feat: implement solution for cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,8 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    args.forEach((arg, i)=> {if((i+1) % 2 !== 0){
+        console.log(arg.slice(2) + ' is ' + args[i+1]);
+    }})
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,10 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+    for(const key in env){
+        if(key.slice(0,4) === 'RSS_'){
+            console.log(key + '=' + env[key]);
+        }
+    }
 };
+
+parseEnv()
