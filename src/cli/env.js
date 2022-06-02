0f5015ffa29dc3c22cc1a3b70cdbb3f77d41dedf
Alexei <lexas427@gmail.com>
2022-06-03 00:09:52 +0300

    feat: implement solution for cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -2,9 +2,9 @@ export const parseEnv = () => {
     const env = process.env;
     for(const key in env){
         if(key.slice(0,4) === 'RSS_'){
-            console.log(key + '=' + env[key] + ';');
+            console.log(key + '=' + env[key]);
         }
     }
 };
 
-parseEnv();
+parseEnv()
