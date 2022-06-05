2579698dc4f93ec77753f11ba9520944cd3847a1
Alexei <lexas427@gmail.com>
2022-06-05 19:34:31 +0300

    feat: add solution for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -2,14 +2,15 @@ import {spawn} from 'child_process'
 import {fileURLToPath} from 'url'
 import {dirname} from 'path'
 
-const spawnChildProcess = async (args) => {
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export const spawnChildProcess = async (args) => {
     const scriptPath = __dirname + '/files/script.js'
     const child = spawn('node', [scriptPath, ...args]);
     child.stdout.on('data', data => process.stdout.write(data));
     process.stdin.on('data', data => child.stdin.write(data));
 
 };
+await spawnChildProcess(['someString', 'anotherString', 1, 22, 333])
 
-spawnChildProcess(['someString', 'anotherString', 1, 22, 333]);
