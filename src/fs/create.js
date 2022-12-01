df40032be24cd97a3ff4b2b441132f156de4001d
Alexei <lexas427@gmail.com>
2022-12-01 13:53:05 +0300

    refactor: create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -6,11 +6,10 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export const create = async () => {
-    await fs.access(__dirname + '/files/fresh.txt', (async err => {
-        if (!err) console.error(Error('FS operation failed'))
-        else await fs.appendFile(__dirname + '/files/fresh.txt', 'I am fresh and young', (() => {
-        }));
-    }))
+    await fs.writeFile(__dirname + '/files/fresh.txt', 'I am fresh and young', {flag: 'wx'}, (async err => {
+        if (err) console.error(Error('FS operation failed'))
+    }));
+
 }
 
 await create();
